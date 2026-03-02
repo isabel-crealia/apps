656ca4ae1d30b3d3114088f79d4e4208df9d765b
isabel-crealia <isabel@octaviodetoledo.com>
2026-03-02 14:12:19 +0100

    Create Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/**
+ * @type {import('tailwindcss').Config}
+ */
+const tailwindConfig = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = tailwindConfig;
